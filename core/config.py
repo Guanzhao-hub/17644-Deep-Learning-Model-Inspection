00c65ebb31a2d14f9eae3607e83489d64f60faee
Ubuntu <ubuntu@ip-172-31-50-171.ec2.internal>
2022-04-02 04:27:31 +0000

    modified train.py to save weights in h5 format

--- core/config.py
+++ core/config.py
@@ -43,8 +43,8 @@ __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
 __C.TRAIN.WARMUP_EPOCHS         = 2
-__C.TRAIN.FISRT_STAGE_EPOCHS    = 20
-__C.TRAIN.SECOND_STAGE_EPOCHS   = 30
+__C.TRAIN.FISRT_STAGE_EPOCHS    = 1
+__C.TRAIN.SECOND_STAGE_EPOCHS   = 1
 __C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_coco_demo.ckpt"
 
 
@@ -52,7 +52,7 @@ __C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_coco_demo.ckpt"
 # TEST options
 __C.TEST                        = edict()
 
-__C.TEST.ANNOT_PATH             = "./data/dataset/voc_test.txt"
+__C.TEST.ANNOT_PATH             = "./data/dataset/mask_test.txt"
 __C.TEST.BATCH_SIZE             = 2
 __C.TEST.INPUT_SIZE             = 544
 __C.TEST.DATA_AUG               = False
@@ -63,9 +63,3 @@ __C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3_coco_demo.ckpt"
 __C.TEST.SHOW_LABEL             = True
 __C.TEST.SCORE_THRESHOLD        = 0.3
 __C.TEST.IOU_THRESHOLD          = 0.45
-
-
-
-
-
-
