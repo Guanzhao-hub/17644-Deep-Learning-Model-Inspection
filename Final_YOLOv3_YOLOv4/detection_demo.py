ba479e2e4723cec70877ded1e9782cbfe5050a32
GoldenWingman <guanzhal@andrew.cmu.edu>
2022-04-12 20:45:47 -0400

    added timer bench mark

--- Final_YOLOv3_YOLOv4/detection_demo.py
+++ Final_YOLOv3_YOLOv4/detection_demo.py
@@ -1,27 +1,30 @@
-#================================================================
+# ================================================================
 #
-#   File name   : detection_demo.py
+#   File name   : detection_custom.py
 #   Author      : PyLessons
-#   Created date: 2020-09-27
+#   Created date: 2020-09-17
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : object detection image and video example
 #
-#================================================================
+# ================================================================
 import os
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
+import time
 import numpy as np
 import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
 
-image_path   = "./IMAGES/maksssksksss12.png"
-# video_path   = "./IMAGES/test.mp4"
-
 yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "./IMAGES/maksssksksss12.png", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
+tic = time.perf_counter()
+with open('fastly_benchmark_yolov3.txt', 'w+') as file:
+    for i in range(20):
+        image_path = "./IMAGES/images/maksssksksss{}.png".format(i + 11)
+        detect_image(yolo, image_path, "./IMAGES/maksssksksss11out.png", input_size=YOLO_INPUT_SIZE, show=True,
+                     CLASSES=TRAIN_CLASSES, rectangle_colors=(255, 0, 0))
+        toc = time.perf_counter()
+        file.write(f"{toc - tic:0.4f}\n")
+    print(f"YOLOv3 finished in {toc - tic:0.4f} seconds")
